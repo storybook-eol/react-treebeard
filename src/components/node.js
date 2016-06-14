5306de865a8549c8ee77703b766b56ba22f5729a
Klaus Dahlén <klaus.dahlen@gmail.com>
2016-06-14 10:38:24 +0300

    Don't clone the node in renderHeader

--- src/components/node.js
+++ src/components/node.js
@@ -59,7 +59,7 @@ class TreeNode extends React.Component {
                 decorators={decorators}
                 animations={animations}
                 style={this.props.style}
-                node={Object.assign({}, this.props.node)}
+                node={this.props.node}
                 onClick={this.onClick}
             />
         );
