1c7215f36c7d7a8dc4131ee34d76b5c7bd29bd25
FUJI Goro (gfx) <gfuji@cpan.org>
2016-08-22 12:41:23 +0900

    remove unused methods originated from react-utils

--- src/utils.js
+++ src/utils.js
@@ -4,17 +4,4 @@ export default {
         if(!Array.isArray(children)){ children = [children]; }
         return children.map(mapper);
     },
-    count: function(children){
-        if(!children){ return 0; }
-        if(!Array.isArray(children)){ return 1; }
-        return children.length;
-    },
-    at: function(children, index){
-        if(!children){ return; }
-        if(!Array.isArray(children)){
-            if(index > 0){ return; }
-            if(index === 0){ return children; }
-        }
-        return children[index];
-    }
 };
