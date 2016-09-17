f9a9f91ccd816f2c43e4ed835b99a5ef1a70281b
Bryce Jacobs <popasquat89@gmail.com>
2016-09-17 01:24:37 -0500

    fix index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    Treebeard: require('./components/treebeard'),
-    decorators: require('./components/decorators'),
-    animations: require('./themes/animations'),
-    theme: require('./themes/default')
+    Treebeard: require('./components/treebeard').default,
+    decorators: require('./components/decorators').default,
+    animations: require('./themes/animations').default,
+    theme: require('./themes/default').default
 };
